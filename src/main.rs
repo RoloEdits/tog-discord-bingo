88f31637915ff0289cbfab18e6e95dc13a52e608
Rolo <roloedits@gmail.com>
2024-05-31 00:13:53 -0700

    feat(font): add more types for missing code points
    - Changed the ggsans font to another implementation.
    - Added Segoe font types to cover additional symbols and emojis.

--- src/main.rs
+++ src/main.rs
@@ -23,18 +23,8 @@ fn main() {
         ..Default::default()
     };
 
-    let mut fonts = FontDefinitions::default();
+    let fonts = register_fonts();
 
-    fonts.font_data.insert(
-        "gg sans".to_owned(),
-        eframe::egui::FontData::from_static(include_bytes!("../gg_sans.ttf")),
-    );
-
-    fonts
-        .families
-        .get_mut(&eframe::egui::FontFamily::Proportional)
-        .unwrap()
-        .insert(0, "gg sans".to_string());
 
     eframe::run_native(
         "Bingo",
@@ -46,6 +36,145 @@ fn main() {
         }),
     )
     .unwrap();
+}
+
+fn register_fonts() -> FontDefinitions {
+    let mut fonts = FontDefinitions::default();
+
+    fonts.font_data.insert(
+        "ggsans_bold".to_owned(),
+        eframe::egui::FontData::from_static(include_bytes!("../fonts/ggsans_bold.ttf")),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .push("ggsans_bold".to_string());
+    
+    fonts.font_data.insert(
+        "ggsans_bolditalic".to_owned(),
+        eframe::egui::FontData::from_static(include_bytes!("../fonts/ggsans_bolditalic.ttf")),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .push( "ggsans_bolditalic".to_string());
+    
+    fonts.font_data.insert(
+        "ggsans_extrabold".to_owned(),
+        eframe::egui::FontData::from_static(include_bytes!("../fonts/ggsans_extrabold.ttf")),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .push( "ggsans_extrabold".to_string());
+    
+    fonts.font_data.insert(
+        "ggsans_extrabolditalic".to_owned(),
+        eframe::egui::FontData::from_static(include_bytes!("../fonts/ggsans_extrabolditalic.ttf")),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .push( "ggsans_extrabolditalic".to_string());
+    
+    fonts.font_data.insert(
+        "ggsans_medium".to_owned(),
+        eframe::egui::FontData::from_static(include_bytes!("../fonts/ggsans_medium.ttf")),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .push( "ggsans_medium".to_string());
+    
+    fonts.font_data.insert(
+        "ggsans_mediumitalic".to_owned(),
+        eframe::egui::FontData::from_static(include_bytes!("../fonts/ggsans_mediumitalic.ttf")),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .push( "ggsans_mediumitalic".to_string());
+    
+    fonts.font_data.insert(
+        "ggsans_normal".to_owned(),
+        eframe::egui::FontData::from_static(include_bytes!("../fonts/ggsans_normal.ttf")),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .push(  "ggsans_normal".to_string());
+    
+    fonts.font_data.insert(
+        "ggsans_normalitalic".to_owned(),
+        eframe::egui::FontData::from_static(include_bytes!("../fonts/ggsans_normalitalic.ttf")),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .push( "ggsans_normalitalic".to_string());
+    
+    fonts.font_data.insert(
+        "ggsans_semibold".to_owned(),
+        eframe::egui::FontData::from_static(include_bytes!("../fonts/ggsans_semibold.ttf")),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .insert(0,  "ggsans_semibold".to_string());
+   
+    fonts.font_data.insert(
+        "ggsans_semibolditalic".to_owned(),
+        eframe::egui::FontData::from_static(include_bytes!("../fonts/ggsans_semibolditalic.ttf")),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .push( "ggsans_semibolditalic".to_string());
+
+    fonts.font_data.insert(
+        "seguihis".to_owned(),
+        eframe::egui::FontData::from_static(include_bytes!("../fonts/seguihis.ttf")),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .push( "seguihis".to_string());
+
+    fonts.font_data.insert(
+        "seguisym".to_owned(),
+        eframe::egui::FontData::from_static(include_bytes!("../fonts/seguisym.ttf")),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .push( "seguisym".to_string());
+
+    fonts
+    
 }
 
 fn include_icon(icon: &[u8]) -> eframe::egui::IconData {
