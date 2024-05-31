ebda8cc9e285a6b2bf564ea066a01c12d5ff1377
Rolo <roloedits@gmail.com>
2024-05-30 23:11:47 -0700

    feat(spreadsheet): support theme colors
    While most theme colors are supported, issues have been observed for
    whites. As this is just an eye candy feature anyways, defaulting to
    white, `FFFFFF`, seems like a sane fallback.

--- src/spreadsheet.rs
+++ src/spreadsheet.rs
@@ -15,15 +15,16 @@ pub fn read<P: AsRef<Path>>(path: P) -> Vec<(u32, String, String, String)> {
         };
 
         let username = cell.get_cell_value().get_value().to_string();
-        // NOTE: hex code includes alpha prefix. Just want the last 6 digits.
-        let hex = cell.get_style().get_font().unwrap().get_color().get_argb();
+        let hex = cell
+            .get_style()
+            .get_font()
+            .unwrap()
+            .get_color()
+            .get_argb_with_theme(workbook.get_theme());
 
-        let color = if hex.is_empty() {
-            // Default to white
-            String::from("#FFFFFF")
-        } else {
-            format!("#{}", &hex[2..])
-        };
+        // NOTE: hex code includes alpha prefix. Just want the last 6 digits.
+        // Default to white if any issues occur.
+        let color = format!("#{}", &hex.get(2..).unwrap_or("FFFFFF"));
 
         let guess = worksheet.get_value((2, row));
 
