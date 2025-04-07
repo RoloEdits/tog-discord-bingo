2581fafb3030152e33f73419e8047ad6a6615e31
Rolo <roloedits@gmail.com>
2025-04-06 22:42:34 -0700

    refactor: swuitch to thread::spawn as scope can block

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -97,13 +97,14 @@ pub trait Game {
     }
 
     fn save_html(&self, path: &Path) {
-        let players = self.players();
-        std::thread::scope(|s| {
-            s.spawn(|| {
-                let mut html = String::new();
+        let players = self.players().to_vec();
+        let path = path.to_path_buf();
 
-                html.push_str(
-                    r#"<!DOCTYPE html>
+        std::thread::spawn(move || {
+            let mut html = String::new();
+
+            html.push_str(
+                r#"<!DOCTYPE html>
             <html>
 
             <head>
@@ -126,72 +127,75 @@ pub trait Game {
                             </tr>
                         </thead>
                         <tbody class="bg-zinc-800 text-white">"#,
-                );
+            );
 
-                for player in players {
-                    write!(html,"<tr class=\" border-b border-zinc-700\">
+            for player in players {
+                write!(html,"<tr class=\" border-b border-zinc-700\">
                                                 <th scope=\"row\" class=\"px-6 py-4 font-medium whitespace-nowrap\" style=\"color: {};\">", player.color ).unwrap();
 
-                    html.push_str(&player.name);
-
-                    html.push_str(
-                        r#"</th>
-                        <td class="px-6 py-4 text-white">"#,
-                    );
-
-                    html.push_str(&player.score.to_string());
-
-                    html.push_str(r"</td></tr>");
-                }
+                html.push_str(&player.name);
 
                 html.push_str(
-                    "</tbody>
+                    r#"</th>
+                        <td class="px-6 py-4 text-white">"#,
+                );
+
+                html.push_str(&player.score.to_string());
+
+                html.push_str(r"</td></tr>");
+            }
+
+            html.push_str(
+                "</tbody>
                     </table>
                 </div>
             </body>
             </html>",
-                );
-                let dir = tempfile::tempdir().unwrap();
+            );
+            let dir = tempfile::tempdir().unwrap();
 
-                let temp_path = dir
-                    .path()
-                    .with_file_name(path.file_name().unwrap())
-                    .with_extension("html");
+            let temp_path = dir
+                .path()
+                .with_file_name(path.file_name().unwrap())
+                .with_extension("html");
 
-                let mut temp = File::create(&temp_path).unwrap();
+            let mut temp = File::create(&temp_path).unwrap();
 
-                temp.write_all(html.as_bytes()).unwrap();
+            temp.write_all(html.as_bytes()).unwrap();
 
-                let browser = Browser::new(headless_chrome::LaunchOptions {idle_browser_timeout: Duration::from_secs(600),..Default::default()}).unwrap();
+            let browser = Browser::new(headless_chrome::LaunchOptions {
+                idle_browser_timeout: Duration::from_secs(600),
+                ..Default::default()
+            })
+            .unwrap();
 
-                let tab = browser.new_tab().unwrap();
+            let tab = browser.new_tab().unwrap();
 
-                tab.navigate_to(temp_path.as_os_str().to_str().unwrap())
-                    .unwrap()
-                    .wait_until_navigated()
-                    .unwrap();
-
-                let element = tab.find_element("tbody").unwrap();
-
-                tab.set_bounds(Bounds::Normal {
-                    left: Some(0),
-                    top: Some(0),
-                    // table width + right vertical scroll bar
-                    width: Some(1_000.0),
-                    height: Some(15_000.0),
-                })
+            tab.navigate_to(temp_path.as_os_str().to_str().unwrap())
+                .unwrap()
+                .wait_until_navigated()
                 .unwrap();
 
-                let bytes = element
-                    .capture_screenshot(CaptureScreenshotFormatOption::Png)
-                    .unwrap();
+            let element = tab.find_element("tbody").unwrap();
 
-                let image =
-                    image::load_from_memory_with_format(&bytes, image::ImageFormat::Png).unwrap();
-                
-                let cropped = image.crop_imm(0, 0, image.width() - 17, image.height());
-                cropped.save(path.with_extension("png")).unwrap();
-            });
+            tab.set_bounds(Bounds::Normal {
+                left: Some(0),
+                top: Some(0),
+                // table width + right vertical scroll bar
+                width: Some(1_000.0),
+                height: Some(15_000.0),
+            })
+            .unwrap();
+
+            let bytes = element
+                .capture_screenshot(CaptureScreenshotFormatOption::Png)
+                .unwrap();
+
+            let image =
+                image::load_from_memory_with_format(&bytes, image::ImageFormat::Png).unwrap();
+
+            let cropped = image.crop_imm(0, 0, image.width() - 17, image.height());
+            cropped.save(path.with_extension("png")).unwrap();
         });
     }
 }
