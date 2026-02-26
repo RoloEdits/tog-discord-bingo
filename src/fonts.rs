b97c1621604e35fceb4a6fdb4e55119e0764d7dd
RoloEdits <RoloEdits@gmail.com>
2026-02-26 11:59:43 -0800

    feat: support korean hangul (#16)

--- src/fonts.rs
+++ src/fonts.rs
@@ -3,7 +3,8 @@ use eframe::egui::FontDefinitions;
 pub static GGSANS: &[u8] = include_bytes!("../fonts/ggsans_semibold.ttf");
 pub static SEGUIHIS: &[u8] = include_bytes!("../fonts/seguihis.ttf");
 pub static SEGUISYM: &[u8] = include_bytes!("../fonts/seguisym.ttf");
-pub static NOTO_SANS: &[u8] = include_bytes!("../fonts/NotoSansSC-SemiBold.ttf");
+pub static NOTO_SANS_CHINENSE: &[u8] = include_bytes!("../fonts/NotoSansSC-SemiBold.ttf");
+pub static NOTO_SANS_KOREAN: &[u8] = include_bytes!("../fonts/NotoSansKR-SemiBold.ttf");
 
 pub fn load() -> FontDefinitions {
     let mut fonts = FontDefinitions::default();
@@ -49,7 +50,7 @@ pub fn load() -> FontDefinitions {
 
     fonts.font_data.insert(
         "noto-sans-sc".to_owned(),
-        eframe::egui::FontData::from_static(NOTO_SANS).into(),
+        eframe::egui::FontData::from_static(NOTO_SANS_CHINENSE).into(),
     );
 
     fonts
@@ -57,6 +58,19 @@ pub fn load() -> FontDefinitions {
         .get_mut(&eframe::egui::FontFamily::Proportional)
         .unwrap()
         .push("noto-sans-sc".to_string());
+
+    // ------------------------------------------------------------
+
+    fonts.font_data.insert(
+        "noto-sans-kr".to_owned(),
+        eframe::egui::FontData::from_static(NOTO_SANS_KOREAN).into(),
+    );
+
+    fonts
+        .families
+        .get_mut(&eframe::egui::FontFamily::Proportional)
+        .unwrap()
+        .push("noto-sans-kr".to_string());
 
     fonts
 }
